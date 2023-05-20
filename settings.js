0e74b1506795a4523daa6ed0c51b79e79c5c91d6
RdeLange <ronald_de_lange@hotmail.com>
2023-05-20 22:39:35 +0200

    Updated Flows

--- settings.js
+++ settings.js
@@ -165,7 +165,7 @@ module.exports = {
      * The following property can be used to specify a different root path.
      * If set to false, this is disabled.
      */
-    //httpAdminRoot: '/admin',
+    httpAdminRoot: '/dnadmin',
 
     /** The following property can be used to add a custom middleware function
      * in front of all admin http routes. For example, to set custom http
